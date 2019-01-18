45fa6f9dde96d76a62e65da0388e5a42bbeb76a1
akashhall <akash.hall@zensar.com>
2019-01-18 19:56:13 +0530

    Updates

--- precache-manifest.c4c48b62338896a10ef925a24e7c9b2f.js
+++ precache-manifest.c4c48b62338896a10ef925a24e7c9b2f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
   },
   {
-    "revision": "3dcfb23f79de30f18ea5",
-    "url": "/react-gh-pages/static/js/main.3dcfb23f.chunk.js"
+    "revision": "49ee922b1d83bdb88d14",
+    "url": "/react-gh-pages/static/js/main.49ee922b.chunk.js"
   },
   {
     "revision": "565555266907a0a6c51e",
     "url": "/react-gh-pages/static/js/1.56555526.chunk.js"
   },
   {
-    "revision": "3dcfb23f79de30f18ea5",
-    "url": "/react-gh-pages/static/css/main.1940652c.chunk.css"
+    "revision": "49ee922b1d83bdb88d14",
+    "url": "/react-gh-pages/static/css/main.26a3b622.chunk.css"
   },
   {
-    "revision": "4903ed58cbb18353b29190d51e46bcda",
+    "revision": "bd6a415c93d8db083f2c3d7bb55557d0",
     "url": "/react-gh-pages/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.0affa8101cb62a92d578866c56c9a10b.js"
+  "/react-gh-pages/precache-manifest.c4c48b62338896a10ef925a24e7c9b2f.js"
 );
 
 workbox.clientsClaim();
