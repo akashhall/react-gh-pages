0bae69c9e1f0934e783c3c777b61ddfd3678b6b7
akashhall <akash.hall@zensar.com>
2019-01-18 18:53:24 +0530

    Updates

---
+++ precache-manifest.901bd5a6f7427eab035c18ea6d3a460d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "90b22d531197509b1d59",
+    "url": "/react-gh-pages/static/js/main.90b22d53.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "90b22d531197509b1d59",
+    "url": "/react-gh-pages/static/css/main.6e3bbfd3.chunk.css"
+  },
+  {
+    "revision": "e404a3f544bacf6c13fcd0c22f4638c5",
+    "url": "/react-gh-pages/index.html"
+  }
+];
